e0d7275a016cc5a94379aaf1467e3c3688d30895
Thisumitha <thisu2006@gmail.com>
2024-07-01 02:41:22 +0530

    asdas

--- src/app/components/sclcarousel/sclcarousel.component.ts
+++ src/app/components/sclcarousel/sclcarousel.component.ts
@@ -1,30 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { NgFor } from '@angular/common';
-import { CarouselComponent, CarouselInnerComponent, CarouselItemComponent, ThemeDirective } from '@coreui/angular';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sclcarousel',
   standalone: true,
-  imports: [ThemeDirective, CarouselComponent, CarouselInnerComponent, NgFor, CarouselItemComponent],
+  imports: [],
   templateUrl: './sclcarousel.component.html',
-  styleUrls: ['./sclcarousel.component.css']
+  styleUrl: './sclcarousel.component.css'
 })
-export class SclcarouselComponent implements OnInit {
-  slides: any[] = new Array(3).fill({ id: -1, src: '', title: '', subtitle: '' });
+export class SclcarouselComponent {
 
-  ngOnInit(): void {
-    this.slides[0] = {
-      src: './assets/carosal1.jpg'
-    };
-    this.slides[1] = {
-      src: './assets/carosal2.jpg'
-    };
-    this.slides[2] = {
-      src: './assets/carosal3.jpg'
-    };
-  }
-
-  onItemChange($event: any): void {
-    console.log('Carousel onItemChange', $event);
-  }
 }
