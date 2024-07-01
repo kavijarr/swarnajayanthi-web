11907f8293e33ca72f7f8d1153e6c1eeee7414d3
kavijarr <kavijakumuditha12@gmail.com>
2024-07-01 22:08:01 +0530

    scroll to element

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-
+import { Component, Renderer2, ElementRef} from '@angular/core';
 @Component({
   selector: 'app-navbar',
   standalone: true,
@@ -8,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
 
+  scrollToComponent(componentId: string) {
+    const element = this.el.nativeElement.ownerDocument.getElementById(componentId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
