7cd9f4da82c4c050f1f4649ccbe4f72ea4a92ce9
Thisumitha <thisu2006@gmail.com>
2024-07-01 10:42:33 +0530

    pina

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { SclcarouselComponent } from '../sclcarousel/sclcarousel.component';
+import { SclVisionComponent } from '../scl-vision/scl-vision.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent,SclcarouselComponent],
+  imports: [NavbarComponent,SclcarouselComponent,SclVisionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
