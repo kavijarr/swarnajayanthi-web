105ce63fbc6f2e98a4b98800d4c437d3289754c9
kavijarr <kavijakumuditha12@gmail.com>
2024-07-01 12:49:17 +0530

    AL streams component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { SclcarouselComponent } from '../sclcarousel/sclcarousel.component';
+import { SubjectStreamsComponent } from '../subject-streams/subject-streams.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent,SclcarouselComponent],
+  imports: [NavbarComponent,SclcarouselComponent,SubjectStreamsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
---
+++ src/app/components/subject-streams/subject-streams.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subject-streams',
+  standalone: true,
+  imports: [],
+  templateUrl: './subject-streams.component.html',
+  styleUrl: './subject-streams.component.css'
+})
+export class SubjectStreamsComponent {
+
+}
